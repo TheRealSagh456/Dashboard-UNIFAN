4f09b7d8a59f1f0747f279fd00c200b848944903
Samuel <samuel.campos@drmais.com.br>
2026-09-09 10:13:44 -0300

    ✨feat: Home-page pré implementação das lógicas

---
+++ Frontend/src/pages/home-page.tsx
@@ -0,0 +1,13 @@
+import { Text } from "../components";
+
+export function HomePage() {
+  return (
+    <div>
+      <div>
+        <Text variant="eyebrow" tone="accent" className="mb-2">
+          Hello World
+        </Text>
+      </div>
+    </div>
+  );
+}
